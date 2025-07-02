5d2d15b1cf38d5e712276faed2fb352f6b17cd91
Ajaysaxena976 <ajaysa9760@gmail.com>
2025-07-02 20:46:58 +0530

    add register controller and bug fixes

--- src/controllers/user.constroller.js
+++ src/controllers/user.constroller.js
@@ -1,9 +1,96 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler(async(requestAnimationFrame, res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+    //kya ky steps honge
+
+    //get user details form frontened 
+    //validation - not empty
+    //check if user already exists(by username or email)
+    //check for images, check for avatar
+    //upload them to cloudinary, avatar
+    //create user object(esi object me data hoga jo db me jayega)- create entery in db 
+     //remobe password and refresh token field from response
+     //check for user creation
+     //return response
+    
+    
+    
+const {fullName, email, username, password} = request.body
+console.log("email:", email);
+
+/* ye simple tarika hai validate krne ka iske niche advance tarika hai ese bhi check kr skte hai 
+
+if(fullName===""){
+        throw new ApiError(400, "fullname is required")
+     }
+})*/
+// validation for checking all fields have data 
+ if(
+        [fullName, email, username, password].some((field)=>
+            field?.trim()==="")
+     ){
+        throw new ApiError(400,"All fields are required")
+     }
+
+
+
+//validation for checking that if user already exist or not
+const existedUser = User.findOne({
+    $or: [{ username },{ email }]
 })
 
-export {registerUser}
+//if user exist then throwing error
+if(existedUser){
+    throw new ApiError(409,"user with email or username already exist")
+}
+
+// handling image 
+const avatarLocalPath = req.files?.avatar[0]?.path;
+const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+//if avatar is not uploaded by user 
+if(!avatarLocalPath){
+    throw new ApiError(400, "Avatar file is required")
+}
+
+// uploading image to cloudinary 
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+if(!avatar){
+    throw new ApiError(400, "Avatar file is required")
+}
+
+
+//if everything is fine then create a object and store in db
+const user = await User.create({
+    fullName,
+    avatar:avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+})
+
+//checking is user created or not  and with .select method we removed password 
+const  createdUser = await User.findById(user._id).select(
+"-password -refreshToken"
+)
+
+if(!createdUser){
+    throw new ApiError(500, "Something went wrong while registering the user ")
+}
+
+//sending api response 
+return res.status(201).json(
+    new ApiResponse(200, createdUser, "user registered successfully")
+)
+
+
+
+})
+export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,24 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/user.constroller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 const router = Router()
 
 /*router.route("/register").post(registerUser)\
         or
 */
-router.post("/register", registerUser);
+router.post("/register",
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]), 
+    registerUser);
 
 
 
